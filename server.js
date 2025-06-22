5b0be99df0cd5018b0031baa9cc17e4ed05206a5
xkret <shbeatsug@gmail.com>
2025-06-22 12:04:43 +0300

    Feat: Make port configurable via environment variable

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const fs = require('fs').promises;
 const path = require('path');
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 const dbPath = path.join(__dirname, 'data', 'db.json');
 const uploadsPath = path.join(__dirname, 'data', 'uploads');
